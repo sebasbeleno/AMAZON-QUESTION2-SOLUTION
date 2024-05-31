4607ea222e34c3ad6074c7a29403a5473aae5595
Sebastian Herrera <sebasbeleno15@gmail.com>
2024-05-30 21:07:55 -0500

    first submition

--- src/index.js
+++ src/index.js
@@ -10,3 +10,36 @@ if (module.hot) {
 }
 
 console.log('DOM Loaded');
+
+const items = document.querySelectorAll(".accordion")
+const multipleCheckbox = document.getElementById('multiselect');
+
+
+items.forEach(item => {
+  console.log(item)
+  const title = item.querySelector(".title-section")
+
+  const expandIcon = item.querySelector(".expand-icon")
+  const collapseIcon = item.querySelector(".collapse-icon")
+  const description = item.querySelector(".description")
+
+  title.addEventListener("click", () => {
+    const isExpanded = item.classList.contains('expanded')
+
+    if (!multipleCheckbox.checked) {
+      items.forEach(i => {
+        i.classList.remove('expanded');
+      });
+    }
+
+    if (!isExpanded) {
+      item.classList.add('expanded');
+      collapseIcon.classList.remove("disable")
+
+    } else {
+      item.classList.remove('expanded');
+      collapseIcon.classList.add("disable")
+    }
+
+  })
+})
